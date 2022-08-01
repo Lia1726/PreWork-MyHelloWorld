64df10cfb21e5a680b6d377a5e43605b4c9678f2
Lia <liasudjana@gmail.com>
2022-08-01 16:41:29 -0500

    1st Commit

--- app/src/main/java/com/example/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/myhelloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myhelloworld
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,10 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //How to reference button
-        val button =findViewById<Button>(R.id.helloButton)
+        val button = findViewById<Button>(R.id.helloButton)
+        val buttonGradient = findViewById<Button>(R.id.buttonGradient)
+
         button.setOnClickListener{
             Log.v("Hello World", "Button Clicked!")
             Toast.makeText(this, "Hello to you!", Toast.LENGTH_SHORT).show()
         }
+
+        buttonGradient.setOnClickListener{
+            Log.v("Button Gradient", "Color Change!")
+            buttonGradient.setBackgroundColor(Color.parseColor("#F5B7BB"));
+
+        }
+
     }
 }
